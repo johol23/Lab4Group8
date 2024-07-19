73a33740534b223d5980f80adb89223be66a17c3
Jessica Nguyen <jnguye44@msudenver.edu>
2024-07-18 22:02:54 -0600

    added star marker and zoomed in map of parked location

--- app/src/main/java/edu/msudenver/cs3013/lab04/DetailFragment.kt
+++ app/src/main/java/edu/msudenver/cs3013/lab04/DetailFragment.kt
@@ -1,28 +1,34 @@
 package edu.msudenver.cs3013.lab04
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class DetailFragment : Fragment(), OnMapReadyCallback {
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var mMap: GoogleMap
+    private var marker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,37 +42,51 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
-    override fun onViewCreated(view: View,
-                               savedInstanceState: Bundle?) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // each time we open the activity and the value will be displayed
+        val viewModel = ViewModelProvider(requireActivity()).get(myViewModel::class.java)
+        viewModel.parkUpdate.observe(viewLifecycleOwner, { updateText(it) })
 
-        val ViewModel =
-            ViewModelProvider(requireActivity()).get(myViewModel::class.java)
-        ViewModel.parkUpdate.observe(viewLifecycleOwner, { updateText(it) })
+        // Initialize the map
+        val mapFragment = childFragmentManager.findFragmentById(R.id.detail_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
-
-
     private fun updateText(latLng: LatLng) {
-        view?.findViewById<TextView>(R.id.detail_fragment)
-            ?.text = "Latitude: ${latLng.latitude}, Longitude: ${latLng.longitude}"
+        view?.findViewById<TextView>(R.id.detail_fragment)?.text = "Latitude: ${latLng.latitude}, Longitude: ${latLng.longitude}"
+        updateMap(latLng)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+    }
+
+    private fun updateMap(latLng: LatLng) {
+        if (::mMap.isInitialized) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+            if (marker == null) {
+                marker = mMap.addMarker(MarkerOptions().position(latLng).title("I'm parked here").icon(getBitmapDescriptorFromVector(R.drawable.star)))
+            } else {
+                marker?.position = latLng
+                marker?.setIcon(getBitmapDescriptorFromVector(R.drawable.star))
+            }
+        }
+    }
+
+    private fun getBitmapDescriptorFromVector(vectorResId: Int): BitmapDescriptor {
+        val vectorDrawable: Drawable? = ContextCompat.getDrawable(requireContext(), vectorResId)
+        vectorDrawable?.setBounds(0, 0, vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight)
+        val bitmap = Bitmap.createBitmap(vectorDrawable!!.intrinsicWidth, vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             DetailFragment().apply {
